2969fc436efcb9581a62954c65e2505977b61ab6
Daniel Leech <dan.t.leech@gmail.com>
2020-04-18 06:43:58 +0100

    WIP

---
+++ tests/LanguageServerWorseReflection/Workspace/WorkspaceIndexTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Phpactor\Extension\LanguageServer\Tests\orseReflection\Workspace;
+
+use PHPUnit\Framework\TestCase;
+
+class WorkspaceIndexTest extends IntegrationTestCase
+{
+    public function 
+}
