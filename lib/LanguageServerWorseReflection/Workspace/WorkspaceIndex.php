88c136ea19dd696e6e674ee4a6fdfec7dc796093
Daniel Leech <dan.t.leech@gmail.com>
2020-04-18 06:39:12 +0100

    WIP

--- lib/LanguageServerWorseReflection/Workspace/WorkspaceIndex.php
+++ lib/LanguageServerWorseReflection/Workspace/WorkspaceIndex.php
@@ -4,9 +4,28 @@ namespace Phpactor\Extension\LanguageServerWorseReflection\Workspace;
 
 use Phpactor\TextDocument\TextDocument;
 use Phpactor\TextDocument\TextDocumentUri;
+use Phpactor\WorseReflection\Core\Name;
+use Phpactor\WorseReflection\Core\Reflection\ReflectionClass;
 
 class WorkspaceIndex
 {
+    /**
+     * @var SourceCodeReflector
+     */
+    private $reflector;
+
+    private $byClass = [];
+    private $byFunction = [];
+
+    public function __construct(SourceCodeReflector $reflector)
+    {
+        $this->reflector = $reflector;
+    }
+
+    public function hasClass(Name $name): bool
+    {
+    }
+
     public function index(TextDocument $textDocument): void
     {
     }
